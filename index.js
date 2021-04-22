89141bac8c05fd18ac0016a1a370578ff5b72611
Eric <eric.d.massoni@gmail.com>
2021-04-22 15:31:57 -0400

    commit tree 27f0eb85f8f88d2a242aadb4f77b291f7e07318f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(array,string){
+    const newArray = array.filter(function(element){
+        return element.toLowerCase() === string.toLowerCase();
+    });
+    return newArray;
+}
+function fuzzyMatch(array,string){
+    const newArray = array.filter(function(element){
+        return element.charAt(0) === string.charAt(0);
+
+    });
+    return newArray;
+}
+function matchName(array,string){
+    const newArray = array.filter(function(element){
+        return element.name === string;
+    });
+    return newArray;
+}
